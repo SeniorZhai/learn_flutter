3b9eecb03ad2e21b692f5cd07bd765f8c3d059b7
zhaitao <370985116@qq.com>
2019-04-14 23:20:07 +0800

    BottomNavigation press handler

--- lib/demo/bottom_navagation_demo.dart
+++ lib/demo/bottom_navagation_demo.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 
-class BottomNavagationDemo extends StatelessWidget {
+class BottomNavagationDemo extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _BottomNavigationBarDemoState();
+  }
+}
+
+class _BottomNavigationBarDemoState extends State<BottomNavagationDemo> {
+  int _currentIndex = 0;
+  void _onTapHandler(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      currentIndex: _currentIndex,
+      onTap: _onTapHandler,
       type: BottomNavigationBarType.fixed,
       fixedColor: Colors.black,
       items: [
