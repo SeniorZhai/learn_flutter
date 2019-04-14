2c862619baf23e97d0a6adc44f43785e6834fc4c
zhaitao <370985116@qq.com>
2019-04-14 22:28:42 +0800

    action button

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,20 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[100],
       appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation',
+          onPressed: () => debugPrint('Navigation button is pressed.'),
+        ),
         title: Text('World'),
-        elevation: 30.0,
+        elevation: 12.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: () => debugPrint('Search button is pressed.'),
+          )
+        ],
       ),
       body: null,
     );
