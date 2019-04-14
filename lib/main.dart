d4e69f04714ee1a966f676ef41ec1338bd0d9791
zhaitao <370985116@qq.com>
2019-04-14 23:12:19 +0800

    Navigation demo

--- lib/main.dart
+++ lib/main.dart
@@ -24,39 +24,38 @@ class Home extends StatelessWidget {
     return DefaultTabController(
         length: 3,
         child: Scaffold(
-          backgroundColor: Colors.grey[100],
-          appBar: AppBar(
-            title: Text('World'),
-            elevation: 12.0,
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.search),
-                tooltip: 'Search',
-                onPressed: () => debugPrint('Search button is pressed.'),
-              )
-            ],
-            bottom: TabBar(
-              unselectedLabelColor: Colors.black38,
-              indicatorColor: Colors.black54,
-              indicatorSize: TabBarIndicatorSize.label,
-              indicatorWeight: 1.0,
-              tabs: <Widget>[
-                Tab(icon: Icon(Icons.local_florist)),
-                Tab(icon: Icon(Icons.change_history)),
-                Tab(icon: Icon(Icons.directions_bike))
+            backgroundColor: Colors.grey[100],
+            appBar: AppBar(
+              title: Text('World'),
+              elevation: 12.0,
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.search),
+                  tooltip: 'Search',
+                  onPressed: () => debugPrint('Search button is pressed.'),
+                )
+              ],
+              bottom: TabBar(
+                unselectedLabelColor: Colors.black38,
+                indicatorColor: Colors.black54,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorWeight: 1.0,
+                tabs: <Widget>[
+                  Tab(icon: Icon(Icons.local_florist)),
+                  Tab(icon: Icon(Icons.change_history)),
+                  Tab(icon: Icon(Icons.directions_bike))
+                ],
+              ),
+            ),
+            body: TabBarView(
+              children: <Widget>[
+                Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+                Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+                Icon(Icons.directions_bike, size: 128.0, color: Colors.black12)
               ],
             ),
-          ),
-          body: TabBarView(
-            children: <Widget>[
-              Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
-              Icon(Icons.change_history, size: 128.0, color: Colors.black12),
-              Icon(Icons.directions_bike, size: 128.0, color: Colors.black12)
-            ],
-          ),
-          drawer: DrawerDemo(),
-          // endDrawer: 右边抽屉
-          bottomNavigationBar: BottomNavagationDemo(),
-        ));
+            drawer: DrawerDemo(),
+            // endDrawer: 右边抽屉
+            bottomNavigationBar: BottomNavagationDemo()));
   }
 }
