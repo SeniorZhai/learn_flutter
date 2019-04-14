ff2662725db7207c28db59f4f6a49ce9bbfed509
zhaitao <370985116@qq.com>
2019-04-14 23:10:52 +0800

    Navigation demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './demo/drawer_demo.dart';
+import './demo/bottom_navagation_demo.dart';
 
 void main() => runApp(MyApp());
 
@@ -23,38 +24,39 @@ class Home extends StatelessWidget {
     return DefaultTabController(
         length: 3,
         child: Scaffold(
-            backgroundColor: Colors.grey[100],
-            appBar: AppBar(
-              title: Text('World'),
-              elevation: 12.0,
-              actions: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.search),
-                  tooltip: 'Search',
-                  onPressed: () => debugPrint('Search button is pressed.'),
-                )
-              ],
-              bottom: TabBar(
-                unselectedLabelColor: Colors.black38,
-                indicatorColor: Colors.black54,
-                indicatorSize: TabBarIndicatorSize.label,
-                indicatorWeight: 1.0,
-                tabs: <Widget>[
-                  Tab(icon: Icon(Icons.local_florist)),
-                  Tab(icon: Icon(Icons.change_history)),
-                  Tab(icon: Icon(Icons.directions_bike))
-                ],
-              ),
-            ),
-            body: TabBarView(
-              children: <Widget>[
-                Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
-                Icon(Icons.change_history, size: 128.0, color: Colors.black12),
-                Icon(Icons.directions_bike, size: 128.0, color: Colors.black12)
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
+            title: Text('World'),
+            elevation: 12.0,
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                tooltip: 'Search',
+                onPressed: () => debugPrint('Search button is pressed.'),
+              )
+            ],
+            bottom: TabBar(
+              unselectedLabelColor: Colors.black38,
+              indicatorColor: Colors.black54,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicatorWeight: 1.0,
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.local_florist)),
+                Tab(icon: Icon(Icons.change_history)),
+                Tab(icon: Icon(Icons.directions_bike))
               ],
             ),
-            drawer: DrawerDemo()
-            // endDrawer: 右边抽屉
-            ));
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+              Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+              Icon(Icons.directions_bike, size: 128.0, color: Colors.black12)
+            ],
+          ),
+          drawer: DrawerDemo(),
+          // endDrawer: 右边抽屉
+          bottomNavigationBar: BottomNavagationDemo(),
+        ));
   }
 }
