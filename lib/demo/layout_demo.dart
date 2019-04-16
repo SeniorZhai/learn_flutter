a3c41dcb6347d4b8a617e4136b57665a5e4d1448
SeniorZhai <developer.zhaitao@gmail.com>
2019-04-16 19:57:51 +0800

    aspect ratio

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -7,53 +7,11 @@ class LayoutDemo extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Stack(
-            alignment: Alignment.topLeft,
-            children: <Widget>[
-              SizedBox(
-                // 固定大小
-                width: 120,
-                height: 200,
-                child: Container(
-                  alignment: Alignment(1.0, -1.0), // x y [-1~1] 子部件对齐位置
-                  decoration: BoxDecoration(
-                      color: Color.fromRGBO(9, 7, 66, 1.0),
-                      borderRadius: BorderRadius.circular(8.0)),
-                  child: Icon(
-                    Icons.access_time,
-                    color: Colors.white,
-                    size: 32.0,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 32,
-              ),
-              SizedBox(
-                width: 100,
-                height: 100,
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Color.fromRGBO(9, 7, 66, 1.0),
-                      borderRadius: BorderRadius.circular(8.0)),
-                  child: Icon(
-                    Icons.access_time,
-                    color: Colors.white,
-                    size: 32.0,
-                  ),
-                ),
-              ),
-              Positioned(
-                //固定位置
-                right: 20.0,
-                top: 20.0,
-                child: Icon(
-                  Icons.accessibility_new,
-                  color: Colors.white,
-                  size: 32.0,
-                ),
-              )
-            ],
+          AspectRatio(
+            aspectRatio: 16.0 / 9.0,
+            child: Container(
+              color: Color.fromRGBO(102, 100, 20, 1.0),
+            ),
           )
         ],
       ),
