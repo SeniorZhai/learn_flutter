636594fb0df3aa9b732072f71ea6493182d6f642
SeniorZhai <developer.zhaitao@gmail.com>
2019-04-16 20:50:41 +0800

    sliver grid

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import '../model/post.dart';
 
 class SliderDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return null;
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverGrid(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 8.0,
+              mainAxisSpacing: 8.0,
+              childAspectRatio: 0.8),
+          delegate:
+              SliverChildBuilderDelegate((BuildContext context, int index) {
+            return Container(
+              child: Image.network(
+                posts[index].imageUrl,
+                fit: BoxFit.cover,
+              ),
+            );
+          }, childCount: posts.length),
+        ),
+      ],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import './demo/drawer_demo.dart';
+import './demo/bottom_navagation_demo.dart';
+import './demo/list_demo.dart';
+import './demo/basic_demo.dart';
+import './demo/layout_demo.dart';
+import './demo/view_demo.dart';
 import './demo/sliver_demo.dart';
 
 void main() => runApp(MyApp());
@@ -17,46 +23,46 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// class Home extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return DefaultTabController(
-//         length: 4,
-//         child: Scaffold(
-//             backgroundColor: Colors.grey[100],
-//             appBar: AppBar(
-//               title: Text('World'),
-//               elevation: 12.0,
-//               actions: <Widget>[
-//                 IconButton(
-//                   icon: Icon(Icons.search),
-//                   tooltip: 'Search',
-//                   onPressed: () => debugPrint('Search button is pressed.'),
-//                 )
-//               ],
-//               bottom: TabBar(
-//                 unselectedLabelColor: Colors.black38,
-//                 indicatorColor: Colors.black54,
-//                 indicatorSize: TabBarIndicatorSize.label,
-//                 indicatorWeight: 1.0,
-//                 tabs: <Widget>[
-//                   Tab(icon: Icon(Icons.local_florist)),
-//                   Tab(icon: Icon(Icons.change_history)),
-//                   Tab(icon: Icon(Icons.directions_bike)),
-//                   Tab(icon: Icon(Icons.view_quilt)),
-//                 ],
-//               ),
-//             ),
-//             body: TabBarView(
-//               children: <Widget>[
-//                 ListViewDome(),
-//                 BasicDemo(),
-//                 LayoutDemo(),
-//                 ViewDemo()
-//               ],
-//             ),
-//             drawer: DrawerDemo(),
-//             // endDrawer: 右边抽屉
-//             bottomNavigationBar: BottomNavagationDemo()));
-//   }
-// }
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+            backgroundColor: Colors.grey[100],
+            appBar: AppBar(
+              title: Text('World'),
+              elevation: 12.0,
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.search),
+                  tooltip: 'Search',
+                  onPressed: () => debugPrint('Search button is pressed.'),
+                )
+              ],
+              bottom: TabBar(
+                unselectedLabelColor: Colors.black38,
+                indicatorColor: Colors.black54,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorWeight: 1.0,
+                tabs: <Widget>[
+                  Tab(icon: Icon(Icons.local_florist)),
+                  Tab(icon: Icon(Icons.change_history)),
+                  Tab(icon: Icon(Icons.directions_bike)),
+                  Tab(icon: Icon(Icons.view_quilt)),
+                ],
+              ),
+            ),
+            body: TabBarView(
+              children: <Widget>[
+                ListViewDome(),
+                BasicDemo(),
+                LayoutDemo(),
+                ViewDemo()
+              ],
+            ),
+            drawer: DrawerDemo(),
+            // endDrawer: 右边抽屉
+            bottomNavigationBar: BottomNavagationDemo()));
+  }
+}
