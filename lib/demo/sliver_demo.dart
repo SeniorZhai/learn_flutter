0f41d09effa28c752d1be06abcda0dc71243806e
SeniorZhai <developer.zhaitao@gmail.com>
2019-04-16 22:43:13 +0800

    slipt sliver grid

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
 import '../model/post.dart';
 
-class SliderDemo extends StatelessWidget {
+class SliverDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: <Widget>[
-        SliverGrid(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 8.0,
-              mainAxisSpacing: 8.0,
-              childAspectRatio: 0.8),
-          delegate:
-              SliverChildBuilderDelegate((BuildContext context, int index) {
-            return Container(
-              child: Image.network(
-                posts[index].imageUrl,
-                fit: BoxFit.cover,
-              ),
-            );
-          }, childCount: posts.length),
-        ),
+        SliverGridDemo()
       ],
+    );
+  }
+}
+
+class SliverGridDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
+          childAspectRatio: 0.8),
+      delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+        return Container(
+          child: Image.network(
+            posts[index].imageUrl,
+            fit: BoxFit.cover,
+          ),
+        );
+      }, childCount: posts.length),
     );
   }
 }
