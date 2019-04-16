21bb031783059818a38382cd6822e86b5db0686d
SeniorZhai <developer.zhaitao@gmail.com>
2019-04-16 22:37:38 +0800

    rename

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SliderDemo(),
+      home: SliverDemo(),
       theme: ThemeData(
           primarySwatch: Colors.yellow,
           highlightColor: Color.fromRGBO(255, 255, 255, 0.13),
