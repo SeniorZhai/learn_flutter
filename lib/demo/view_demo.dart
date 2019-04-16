2bda110656cee1aa5c7b030f8b0f79e56a808d60
SeniorZhai <developer.zhaitao@gmail.com>
2019-04-16 20:23:42 +0800

    page view builder

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -1,6 +1,41 @@
 import 'package:flutter/material.dart';
+import '../model/post.dart';
 
 class ViewDemo extends StatelessWidget {
+  Widget _pagetItemBuilder(BuildContext context, int index) {
+    return Stack(children: <Widget>[
+      SizedBox.expand(
+          child: Image.network(
+        posts[index].imageUrl,
+        fit: BoxFit.cover,
+      )),
+      Positioned(
+        bottom: 8.0,
+        left: 8.0,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              posts[index].title,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(posts[index].author)
+          ],
+        ),
+      )
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      itemCount: posts.length,
+      itemBuilder: _pagetItemBuilder,
+    );
+  }
+}
+
+class PageViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageView(
@@ -8,7 +43,10 @@ class ViewDemo extends StatelessWidget {
       reverse: true, // 倒序
       onPageChanged: (index) => debugPrint('$index'), // 回调
       controller: PageController(
-          initialPage: 1, keepPage: false, viewportFraction: 0.80 // 页占显示区域的比例),
+          initialPage: 1, // 初始位置
+          keepPage: false, // 不记住位置
+          viewportFraction: 0.80 // 页占显示区域的比例
+          ),
       scrollDirection: Axis.vertical,
       children: <Widget>[
         Container(
