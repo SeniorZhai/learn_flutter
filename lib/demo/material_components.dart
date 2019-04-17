54769a4a2fd37e0f0f2022c2cc369fa1e8bb5cdb
SeniorZhai <developer.zhaitao@gmail.com>
2019-04-17 20:34:53 +0800

    Flatbutton

--- lib/demo/material_components.dart
+++ lib/demo/material_components.dart
@@ -11,10 +11,52 @@ class MaterialComponents extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           ListItem(
+            title: 'Button',
+            page: ButtonDemo(),
+          ),
+          ListItem(
             title: 'FloatingActionButton',
             page: FloatingActionButtonDemo(),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ButtonDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FloatingActionButtonDemo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FlatButton(
+                  child: Text('Button'),
+                  onPressed: () {},
+                  splashColor: Colors.grey,
+                  textColor: Theme.of(context).accentColor,
+                ),
+                FlatButton.icon(
+                  icon: Icon(Icons.add),
+                  label: Text('button'),
+                  onPressed: () {},
+                  splashColor: Colors.grey,
+                  textColor: Theme.of(context).accentColor,
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
