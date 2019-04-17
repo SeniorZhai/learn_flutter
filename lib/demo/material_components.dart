42e036495c11d6b5411aa0a9afcf1d3585869a29
SeniorZhai <developer.zhaitao@gmail.com>
2019-04-17 20:28:03 +0800

    FloatingActionButton

---
+++ lib/demo/material_components.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class MaterialComponents extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MaterialComponents'),
+        elevation: 0.0,
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListItem(
+            title: 'FloatingActionButton',
+            page: FloatingActionButtonDemo(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FloatingActionButtonDemo extends StatelessWidget {
+  final Widget _floatingActionButton = FloatingActionButton(
+    onPressed: () {},
+    child: Icon(Icons.add),
+    elevation: 0.0,
+    backgroundColor: Colors.black87,
+    shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+  );
+  final Widget _floatingActionButtonExtened = FloatingActionButton.extended(
+    onPressed: () {},
+    icon: Icon(Icons.add),
+    label: Text('Add'),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FloatingActionButtonDemo'),
+        elevation: 0.0,
+      ),
+      floatingActionButton: _floatingActionButtonExtened,
+    );
+  }
+}
+
+class ListItem extends StatelessWidget {
+  final String title;
+  final Widget page;
+  ListItem({this.title, this.page});
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      onTap: () {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => page));
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import './demo/view_demo.dart';
 import './demo/sliver_demo.dart';
 import './demo/navigator_demo.dart';
 import './demo/form_demo.dart';
+import './demo/material_components.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,11 +17,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/form',
+      initialRoute: '/mdc',
       routes: {
         '/': (context) => Home(),
         '/about': (context) => Page(title: 'About'),
-        '/form': (context) => FormDemo()
+        '/form': (context) => FormDemo(),
+        '/mdc': (context) => MaterialComponents()
       },
       theme: ThemeData(
           primarySwatch: Colors.yellow,
