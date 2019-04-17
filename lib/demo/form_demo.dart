a5b43d29bbdceeb5aa71c3f2e826d1ca4ce0ad17
SeniorZhai <developer.zhaitao@gmail.com>
2019-04-17 19:34:24 +0800

    text field

--- lib/demo/form_demo.dart
+++ lib/demo/form_demo.dart
@@ -7,7 +7,13 @@ class FormDemo extends StatelessWidget {
       body: Theme(
         // data: ThemeData(primaryColor: Colors.blue),
         data: Theme.of(context).copyWith(primaryColor: Colors.black),
-        child: ThemeDemo(),
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[TextFieldDemo()],
+          ),
+        ),
       ),
     );
   }
@@ -26,9 +32,40 @@ class TextFieldDemo extends StatefulWidget {
 }
 
 class _TextFieldDemoState extends State<TextFieldDemo> {
+  final textEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    textEditingController.text = 'hi';
+    textEditingController.addListener(() {
+      debugPrint('input ${textEditingController.text}');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: textEditingController,
+      decoration: InputDecoration(
+          icon: Icon(Icons.subject),
+          labelText: 'Title',
+          hintText: "Enter the post titls",
+          // border: InputBorder.none,
+          // border: OutlineInputBorder(),
+          filled: true),
+      // onChanged: (value) {
+      //   debugPrint('input $value');
+      // },
+      onSubmitted: (value) {
+        debugPrint('submit $value');
+      },
       cursorColor: Colors.red,
     );
   }
