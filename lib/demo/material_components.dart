e68ae52086f504bc2d722ae1504ec1167aa10957
SeniorZhai <developer.zhaitao@gmail.com>
2019-04-17 20:49:25 +0800

    Button container

--- lib/demo/material_components.dart
+++ lib/demo/material_components.dart
@@ -26,6 +26,10 @@ class MaterialComponents extends StatelessWidget {
             title: 'OutlineButton',
             page: OutlineButtonDemo(),
           ),
+          ListItem(
+            title: 'FixedButton',
+            page: FixedButtonDemo(),
+          ),
         ],
       ),
     );
@@ -149,6 +153,61 @@ class OutlineButtonDemo extends StatelessWidget {
                   onPressed: () {},
                   splashColor: Colors.grey,
                   textColor: Theme.of(context).accentColor,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FixedButtonDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FloatingActionButtonDemo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  // 占据所有可用空间
+                  child: OutlineButton(
+                    child: Text('Button'),
+                    onPressed: () {},
+                    splashColor: Colors.red[100],
+                    borderSide: BorderSide(color: Colors.red),
+                    color: Theme.of(context).accentColor,
+                    textColor: Colors.black,
+                    shape: StadiumBorder(),
+                    highlightedBorderColor: Colors.grey,
+                  ),
+                ),
+                SizedBox(
+                  width: 12,
+                ),
+                Expanded(
+                  flex: 2,
+                  // 占据所有可用空间
+                  child: OutlineButton(
+                    child: Text('Button'),
+                    onPressed: () {},
+                    splashColor: Colors.red[100],
+                    borderSide: BorderSide(color: Colors.red),
+                    color: Theme.of(context).accentColor,
+                    textColor: Colors.black,
+                    shape: StadiumBorder(),
+                    highlightedBorderColor: Colors.grey,
+                  ),
                 )
               ],
             )
