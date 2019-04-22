8a3dc0a0face4285d0525873aafaeef364edfc3e
SeniorZhai <developer.zhaitao@gmail.com>
2019-04-22 14:25:38 +0800

    Animation value

--- lib/demo/animation/animation_demo.dart
+++ lib/demo/animation/animation_demo.dart
@@ -12,7 +12,32 @@ class AnimationDemoHome extends StatefulWidget {
   _AnimationDemoHomeState createState() => _AnimationDemoHomeState();
 }
 
-class _AnimationDemoHomeState extends State<AnimationDemoHome> {
+class _AnimationDemoHomeState extends State<AnimationDemoHome>
+    with TickerProviderStateMixin {
+  AnimationController animationDemoController;
+  @override
+  void initState() {
+    super.initState();
+    animationDemoController = AnimationController(
+      value: 32.0,
+      lowerBound: 0.0,
+      upperBound: 100.0,
+      duration: Duration(milliseconds: 1000),
+      vsync: this,
+    );
+    animationDemoController.addListener(() {
+      // print('${animationDemoController.value}');
+      setState(() {});
+    });
+    // animationDemoController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationDemoController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +45,14 @@ class _AnimationDemoHomeState extends State<AnimationDemoHome> {
         title: Text('Animation'),
         elevation: 0.0,
       ),
-      body: Center(),
+      body: Center(
+        child: ActionChip(
+          label: Text('${animationDemoController.value}'),
+          onPressed: () {
+            animationDemoController.forward();
+          },
+        ),
+      ),
     );
   }
 }
