dcc80cf4467e1c6fc3152f1f6a85be4f16d6f471
SeniorZhai <developer.zhaitao@gmail.com>
2019-04-22 14:07:31 +0800

    http

--- lib/demo/http/http_demo.dart
+++ lib/demo/http/http_demo.dart
@@ -25,21 +25,72 @@ class _HttpDemoHomeState extends State<HttpDemoHome> {
   @override
   void initState() {
     super.initState();
-    fetchPost();
+    // fetchPosts().then((list) => {print(list)});
   }
 
-  fetchPost() async {
+  Future<List<Post>> fetchPosts() async {
     final response = await http
         .get('https://my-json-server.typicode.com/SeniorZhai/test_api/posts');
 
     print('statusCode: ${response.statusCode}');
     // print('body: ${response.body}');
+
     final postJsonConverted = json.decode(response.body);
-    print(postJsonConverted.length);
+    final list =
+        postJsonConverted.map<Post>((item) => Post.fromJson(item)).toList();
+    return list;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return FutureBuilder(
+        future: fetchPosts(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          print('data: ${snapshot.data}');
+          print('connectionState: ${snapshot.connectionState}');
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: Text('loading...'),
+            );
+          }
+
+          return ListView(
+            children: snapshot.data.map<Widget>((item) {
+              return ListTile(
+                title: Text(item.title),
+                subtitle: Text(item.author),
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(item.imageUrl),
+                ),
+              );
+            }).toList(),
+          );
+        });
   }
+}
+
+class Post {
+  final String title;
+  final String description;
+  final String author;
+  final String imageUrl;
+
+  Post(
+    this.title,
+    this.description,
+    this.author,
+    this.imageUrl,
+  );
+
+  Post.fromJson(Map json)
+      : title = json['title'],
+        description = json['description'],
+        author = json['author'],
+        imageUrl = json['imageUrl'];
+
+  Map toJson() => {
+        'title': title,
+        'descritpion': description,
+      };
 }
