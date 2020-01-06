3e9a8003638122fd108bc0a536b94b791acb1104
SeniorZhai <developer.zhaitao@gmail.com>
2020-01-06 18:27:20 +0800

    Story UI

--- lib/speed_code/speed_code.dart
+++ lib/speed_code/speed_code.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import './login/login_page.dart';
 import '../list_item.dart';
+import './story/story_ui.dart';
 
 class SpeedCode extends StatelessWidget {
   List<Widget> list = new List<Widget>()
-    ..add(ListItem(title: 'LoginPage', page: LoginPage()));
+    ..add(ListItem(title: 'LoginPage', page: LoginPage()))
+    ..add(ListItem(title: 'StoryUI', page: StoryUI()));
   @override
   Widget build(BuildContext context) {
     return Scaffold(
---
+++ lib/speed_code/story/custom_icons.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class CustomIcons {
+  static const IconData menu = IconData(0xe900, fontFamily: "StoryIcons");
+  static const IconData option = IconData(0xe902, fontFamily: "StoryIcons");
+}
